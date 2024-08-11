cd6e568fbe72b8d1bb184c7029bcda50b3fe2355
ManishJoc14 <manishjoc14@gmail.com>
2024-08-11 17:03:52 +0545

    added metadata  and favicon

--- tailwind-portfolio/src/app/layout.tsx
+++ tailwind-portfolio/src/app/layout.tsx
@@ -4,9 +4,19 @@ import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
 
 export const metadata: Metadata = {
-  title: "Tailwind Portfolio",
+  title: "Manish Joshi - Portfolio",
   description:
     "Best portfolio made with Nextjs and Framer Motion, TailwindCSS and with unique Dark theme",
+  openGraph: {
+    images: [
+      {
+        url: "/tailwind-portfolio.png",
+        width: 1200,
+        height: 630,
+        alt: "Manish Joshi - Portfolio",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -16,11 +26,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:image" content="/tailwind-portfolio.png" />
+        <meta property="og:image:type" content="image/x-icon" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content="Manish Joshi - Portfolio" />
+      </head>
       <body>
         <div className="min-h-screen bg-primary font-mono">
           <div className="mx-auto px-8 py-4"></div>
           <Header />
-            {children}
+          {children}
           <Footer />
         </div>
       </body>
